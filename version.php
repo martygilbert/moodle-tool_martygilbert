bb94b1f77f1e3aa5c43582722f08bbd9e8b75132
Marty Gilbert <martygilbert@gmail.com>
2019-10-10 11:13:38 -0400

    removed extra whitespace

--- version.php
+++ version.php
@@ -13,15 +13,15 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
- 
+
 /**
  * @package   tool_martygilbert
  * @copyright 2019, Marty Gilbert <martygilbert@gmail.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
- 
+
 defined('MOODLE_INTERNAL') || die();
- 
+
 $plugin->version = 2019101001;
 $plugin->requires = 20190801;
 $plugin->component = 'tool_martygilbert';
